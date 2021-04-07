cd78c57763735ff0f21f597556c32fd3e2420d4c
Thomas <thombsaway@gmail.com>
2021-04-07 21:47:17 +1000

    Stub Account class, with public fields.

---
+++ ITServiceDesk/src/Account.java
@@ -0,0 +1,16 @@
+import jdk.nashorn.internal.objects.annotations.Constructor;
+
+public class Account {
+    
+    public String email;
+    public String name;
+    public String number;
+    public String password;
+
+    Account(String email, String name, String number, String password) {
+        this.email = email;
+        this.name = name;
+        this.number = number; 
+        this.password = password;
+    }
+}
