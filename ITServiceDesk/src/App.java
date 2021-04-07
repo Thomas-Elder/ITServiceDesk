5e25f6f9e262927e6e338eba7c1f29bb6fdaf4f1
Thomas <thombsaway@gmail.com>
2021-04-07 21:39:58 +1000

    Create main application loop, and stub prints for menu options.

--- ITServiceDesk/src/App.java
+++ ITServiceDesk/src/App.java
@@ -1,5 +1,28 @@
+
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Welcom to Cinco IT Service Desk!");
+
+        char option = '0';
+
+        while (option != 'X') {
+
+            // TODO print action options
+            System.out.println("Please select and option:");
+            System.out.println("1 - Create an Account");
+            System.out.println("2 - Log in");
+            System.out.println("3 - Create a Ticket");
+            System.out.println("X - Exit");
+
+            // Get input
+            Scanner sc = new Scanner(System.in);
+
+            // TODO Wrap this to validate input
+            option = sc.nextLine().toCharArray()[0];
+            
+            // TODO switch to handle option selection
+        }        
     }
 }
