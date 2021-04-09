588b545b40e2b697cdf06258bca3534f87ca63c5
Thomas <thombsaway@gmail.com>
2021-04-09 15:51:18 +1000

    Stub Database class

---
+++ ITServiceDesk/src/Database.java
@@ -0,0 +1,14 @@
+import java.util.Dictionary;
+
+public class Database {
+    
+    Dictionary<String, Account> dict = new Dictionary<String, Account>(); 
+
+    Database(){
+
+    }
+
+    public Account getAccount(String email){
+
+    }
+}
