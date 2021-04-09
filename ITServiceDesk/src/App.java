f4da4372a2c7e50e2087788e4c790cceaf96b78f
Thomas <thombsaway@gmail.com>
2021-04-09 14:34:32 +1000

    Create arraylist for Accounts and add hardcoded technician Accounts

--- ITServiceDesk/src/App.java
+++ ITServiceDesk/src/App.java
@@ -4,9 +4,19 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         
+        // Create list for accounts
+        ArrayList <Account> accountList= new ArrayList<Account>(); 
+
+        // Create technician accounts
+        accountList.add(new Account("", "Harry Style", "", ""));
+        accountList.add(new Account("", "Niall Horan", "", ""));
+        accountList.add(new Account("", "Liam Payne", "", ""));
+        accountList.add(new Account("", "Louis Tomlinson", "", ""));
+        accountList.add(new Account("", "Zayn Malik", "", ""));
+
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Welcom to Cinco IT Service Desk!");
+        System.out.println("Welcome to Cinco IT Service Desk!");
 
         char option = '0';
 
