0098589acef75862ee675a2e1dabc458a0da23de
Thomas Elder <thombsaway@gmail.com>
2021-04-11 12:10:22 +1000

    Add separate lists for Staff and Technician accounts to make access easier.

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,18 +1,45 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Database {
     
-    HashMap<String, Account> accountHashMap = new HashMap<String, Account>(); 
+    HashMap<String, Staff> staffAccountsHashMap = new HashMap<String, Staff>();
+    HashMap<String, Technician> technicianAccountsHashMap = new HashMap<String, Technician>();
+    List<Ticket> ticketList = new ArrayList<Ticket>(); 
 
     Database(){
 
     }
 
-    public void addAccount(Account newAccount){
-        accountHashMap.put(newAccount.email, newAccount);
+    public void addStaffAccount(Staff newStaffAccount){
+        staffAccountsHashMap.put(newStaffAccount.email, newStaffAccount);
     }
 
-    public Account getAccount(String email){
-        return accountHashMap.get(email);
+    public Staff getStaffAccount(String email){
+        return staffAccountsHashMap.get(email);
+    }
+
+    public void addTechnicianAccount(Technician newTechnicianAccount){
+        technicianAccountsHashMap.put(newTechnicianAccount.email, newTechnicianAccount);
+    }
+
+    public Technician getTechnicianAccount(String email){
+        return technicianAccountsHashMap.get(email);
+    }
+
+    public void addTicket(Ticket newTicket){
+        ticketList.add(newTicket);
+    }
+
+    public List<Ticket> getTickets() {
+        return ticketList;
+    }
+
+    public List<Ticket> getTickets(Technician technician) {
+        return this.ticketList.stream()
+        .filter(t -> t.assignedTechnician.equals(technician))
+        .collect(Collectors.toList());
     }
 }
