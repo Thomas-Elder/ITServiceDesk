cc0ec87a74a1f615d896d9d2c999e578d99911e8
Thomas <thombsaway@gmail.com>
2021-04-15 13:54:34 +1000

    Simplify ITSystem class

--- src/main/java/ITSystem.java
+++ src/main/java/ITSystem.java
@@ -1,15 +1,11 @@
 public class ITSystem {
     public String OS;
-    public String system;
     public String program;
     public String version;
-    public String description;
 
-    ITSystem(String OS, String system, String program, String version, String description) {
+    ITSystem(String OS, String program, String version) {
         this.OS = OS;
-        this.system = system;
         this.program = program;
         this.version = version;
-        this.description = description;
     }
 }
